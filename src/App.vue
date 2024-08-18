501f5ca9380c9234f72c37bd3ad705723a535e2e
Vincent-the-gamer <erxianqiaottjt@163.com>
2024-08-18 16:06:09 +0800

    chore: init.

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import Girl from "./components/Girl.vue"
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+</script>
+
+<template>
+  <div class="description">
+    <h3>
+      Demo by <a href="https://github.com/Vincent-the-gamer">Vincent-the-gamer</a>
+    </h3>
+  </div>
+  <div class="model">
+    <TresCanvas shadows alpha>
+      <TresPerspectiveCamera :position="[2, 4, 7]" 
+                             :look-at="[0, 2, 0]"
+      />
+      <OrbitControls :target="[0, 2, 0]"/>
+      <Suspense>
+        <Girl />
+      </Suspense>
+      <TresGridHelper />
+    </TresCanvas>
+  </div>
+</template>
+
+<style scoped>
+.description {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: fit-content;
+  left: 0;
+  bottom: 0;
+  z-index: 2;
+  background-color: rgba(50, 50, 50, 0.5);
+  color: white;
+}
+
+a {
+  text-decoration: none;
+  color: pink;
+}
+
+.model {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+}
+</style>
---
+++ src/components/Girl.vue
@@ -0,0 +1,18 @@
+<template>
+    <primitive :object="model" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+import { useAnimations, useGLTF } from '@tresjs/cientos'
+
+const { scene: model, animations } = await useGLTF("/models/elaina_anim_model/scene.gltf")
+
+const { actions } = useAnimations(animations, model)
+
+const currentAction = ref(actions["rigAction.001"])
+
+currentAction.value.play()
+</script>
+
+<style scoped></style>
